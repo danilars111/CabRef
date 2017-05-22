56bc0f56ad59bda57853d0e39629b75824de1341
Joerg Lenhard <joerg.lenhard@kau.se>
2017-05-22 10:03:51 +0200

    Add JabRef sources

---
+++ Jabref/src/main/java/org/jabref/gui/fieldeditors/FieldNameLabel.java
@@ -0,0 +1,18 @@
+package org.jabref.gui.fieldeditors;
+
+import javafx.scene.control.Label;
+
+import org.jabref.model.entry.FieldName;
+
+public class FieldNameLabel extends Label {
+
+    public FieldNameLabel(String fieldName) {
+        super(FieldName.getDisplayName(fieldName));
+
+        // TODO: style!
+        //setVerticalAlignment(SwingConstants.TOP);
+        //setForeground(GUIGlobals.ENTRY_EDITOR_LABEL_COLOR);
+        //setBorder(BorderFactory.createEmptyBorder());
+    }
+
+}
